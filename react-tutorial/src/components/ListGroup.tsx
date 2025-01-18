6703612070597dccb1fce30f8a53cc6fc500958c
Hugo Retail <hugo.retail@hotmail.com>
2025-01-18 12:37:31 +0100

    passing functions via props: done

--- react-tutorial/src/components/ListGroup.tsx
+++ react-tutorial/src/components/ListGroup.tsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
-} 
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
+}
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -16,9 +18,17 @@ function ListGroup({ items, heading }: Props) {
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item' }
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => { selectedIndex === index ? setSelectedIndex(-1) : setSelectedIndex(index) }}
+            onClick={() => {
+              selectedIndex === index
+                ? setSelectedIndex(-1)
+                : (setSelectedIndex(index), onSelectItem(item));
+            }}
           >
             {item}
           </li>
